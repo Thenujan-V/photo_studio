36512179196c50bf14febce7e6589608fb945614
Nilushiya <nilushiyak@gmail.com>
2025-05-01 11:11:25 +0530

    frontend

---
+++ frontend/src/pages/Services.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import AppNavbar from '../components/Navbar'
+
+const Services = () => {
+  return (
+    <div>
+        <AppNavbar />
+        Services
+    </div>
+  )
+}
+
+export default Services
